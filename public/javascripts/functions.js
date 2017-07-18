d1ea50acfc8861c3b221e7654296cd8c8045003b
patriki <patriki@MacBook-Pro-de-patriki.local>
2017-07-18 17:22:24 +0200

    script tag fixed

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,27 +1,28 @@
+/* jshint esversion: 6 */
 
 function showRecipes (response) {
   $(".output").html('');
-  console.log(response.matches)
+  console.log(response.matches);
   response.matches.forEach((el,index)=> {
     $(".output").append(`
-                    <div class=recipe id="recipe${index}"> 
+                    <div class=recipe id="recipe${index}">
                         <div class="image">
-                            <img src=${el.smallImageUrls[0].replace(/s90$/,"s350")}> 
+                            <img src=${el.smallImageUrls[0].replace(/s90$/,"s350")}>
                         </div>
                         <div class="info">
-                            <span class='title'> Recipe: </span> ${el.recipeName} <br> 
-                            <span class='title'> Ingredients: </span> ${[...el.ingredients]} <br> 
+                            <span class='title'> Recipe: </span> ${el.recipeName} <br>
+                            <span class='title'> Ingredients: </span> ${[...el.ingredients]} <br>
                             <span class='title'> Rating: ${el.rating}
                         </div>
-                    
+
                     </div>
                     `)
-  
+
 
   $.ajax( {
       url:`http://api.yummly.com/v1/api/recipe/${el.id}?_app_id=7b6372ab&_app_key=446dc1e04dcdedcfe61b2515ec058e88`,
       method:"GET",
-      success: function(res) { 
+      success: function(res) {
           console.log(res);
           $(`#recipe${index}`).append(`<span class= 'title'> Sources: ${res.source.sourceRecipeUrl} </span>`)
       },
@@ -42,7 +43,7 @@ function getParameters (filter) {
 }
 
 
-function createForms (filter,selector) {
+function createForms (filter, selector) {
 $.ajax({
     url: `http://api.yummly.com/v1/api/metadata/${filter}?_app_id=7b6372ab&_app_key=446dc1e04dcdedcfe61b2515ec058e88`,
     method:"GET",
@@ -58,9 +59,9 @@ function displayFilter(response,filter,selector) {
       jsonDocs.forEach ((el)=> {
 
           $(`.${filter}`).append(`<input type="checkbox" name="each${filter}" value=${el.searchValue.replace(" ","%20")}>${el[selector]}`)
-          
+
       })
-    
+
   }
 
 function handleError (error) {
@@ -73,7 +74,7 @@ function extractMetadata(response) {
     })
     response = response.join()
     response = response.split(")")[0];
-        
+
     return JSON.parse(response);
 
 }
